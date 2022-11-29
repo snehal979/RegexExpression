655d934bcebd4fc3d2022d027934b863cacb640c
Snehal979 <bansodsnehal535@gmail.com>
2022-11-29 20:34:13 +0530

    (Snehal)Add: Validation of First name

--- RegexExpression/RegexExpression/Program.cs
+++ RegexExpression/RegexExpression/Program.cs
@@ -5,6 +5,14 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("regex Experssion");
+            Console.WriteLine("-------------");
+            Validation Code = new Validation();
+           
+            string input = "Snehal";
+            Code.Test(input);
+
+
+
             Console.ReadLine();
         }
     }
---
+++ RegexExpression/RegexExpression/Validation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExpression
+{
+    public class Validation
+    {
+        const string First_Name = "^[a-zA-Z]{3,}";
+        public void Test(string input)
+        {
+            if (Regex.IsMatch(input, First_Name))
+                Console.WriteLine("First name matches");
+            else
+                Console.WriteLine("First name regex failed");
+        }
+    }
+}
